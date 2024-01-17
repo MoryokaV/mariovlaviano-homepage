dac2b3e15890efeb310878ea06fa99b472375b1b
Moryoka <mariovlaviano2005@yahoo.com>
2024-01-17 21:04:06 +0200

    remove next favicon & add logo

--- components/logo.js
+++ components/logo.js
@@ -1,6 +1,6 @@
-import Link from "next/link"
-import { Text, useColorModeValue } from "@chakra-ui/react"
-import styled from "@emotion/styled"
+import Link from 'next/link'
+import { Text, Box, useColorModeValue } from '@chakra-ui/react'
+import styled from '@emotion/styled'
 
 const LogoBox = styled.span`
   font-weight: bold;
@@ -9,24 +9,36 @@ const LogoBox = styled.span`
   align-items: center;
   line-height: 20px;
   padding: 10px;
+  gap: 8px;
 
-  > svg {
-    transition: 200ms ease;
+  &:hover .bracket-left {
+    transform: translateX(-3px);
   }
 
-  &:hover > svg {
-    transform: rotate(20deg);
+  &:hover .bracket-right {
+    transform: translateX(3px);
   }
+`
+
+const Bracket = styled.div`
+  transition: all 0.3s ease;
+  font-weight: 400;
 `
 
 const Logo = () => {
   return (
     <Link href="/" scroll={false}>
       <LogoBox>
+        <Box display="flex" gap="3px">
+          <Bracket className="bracket-left">&#123;</Bracket>
+          <Text fontSize="14px" fontWeight="300">
+            MV
+          </Text>
+          <Bracket className="bracket-right">&#125;</Bracket>
+        </Box>
         <Text
-          color={useColorModeValue("gray.800", "whiteAlpha.900")}
+          color={useColorModeValue('gray.800', 'whiteAlpha.900')}
           fontFamily='M PLUS Rounded 1c", sans-serif'
-          fontWeight="bold"
           ml={3}
         >
           Mario Vlaviano
