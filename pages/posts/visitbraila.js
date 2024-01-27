ff29434dcd9a95792faf689609f6d9bc8e5331b2
Moryoka <mariovlaviano2005@yahoo.com>
2024-01-27 19:05:15 +0200

    Add new video link

--- pages/posts/visitbraila.js
+++ pages/posts/visitbraila.js
@@ -149,7 +149,7 @@ const Post = () => {
           <iframe
             width="100%"
             height="320"
-            src="https://www.youtube.com/embed/XpVzETwet1c?si=lc3pOHmMTce1JIJG"
+            src="https://www.youtube.com/embed/Rgm_6V8ux1U?si=F1dBcpuYXYnETJ8v"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share; fullscreen"
